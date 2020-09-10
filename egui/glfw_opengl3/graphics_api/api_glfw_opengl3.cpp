adcfcc56aaf688571265dbdf97fda0e9b42bd077
mint pekka <pekka.lehtikoski@gmail.com>
2020-09-10 09:14:49 -0400

    Noto font

--- egui/glfw_opengl3/graphics_api/api_glfw_opengl3.cpp
+++ egui/glfw_opengl3/graphics_api/api_glfw_opengl3.cpp
@@ -130,6 +130,9 @@ int duudeli(void)
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+
+    io.Fonts->AddFontFromFileTTF("/coderoot/bluetree/egui/fonts/NotoSans-Medium.ttf", 20.0f);
+
     //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
     //IM_ASSERT(font != NULL);
 
